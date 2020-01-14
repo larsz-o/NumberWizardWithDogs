937b07c2e011a61d933e94a64dafd81a4fb8b9a8
Lars <larsmackenzie@gmail.com>
2020-01-14 15:33:11 -0500

    nav back to play again

--- Assets/SceneLoader.cs
+++ Assets/SceneLoader.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
-    
     public void LoadNextScene()
     {
         // find out current scene
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         // load the next one
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene(){
+        SceneManager.LoadScene(0);
     }
 }
